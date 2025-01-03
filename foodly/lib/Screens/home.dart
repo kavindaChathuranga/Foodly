2087d6740c646a063fbc5f8576cd808224e7ab83
ChamikaNiroshan <M.ChamikaNiroshan@gmail.com>
2025-01-04 03:45:38 +0530

    Added search feature
    Added search feature

--- foodly/lib/Screens/home.dart
+++ foodly/lib/Screens/home.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Required to load assets
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:foodly/Recipes/recipe_data.dart'; // Import the recipe data
 
 class FoodlyApp extends StatefulWidget {
   const FoodlyApp({super.key});
@@ -100,7 +101,46 @@ class _FoodlyAppState extends State<FoodlyApp> {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Map<String, dynamic>>> recipes;
+  TextEditingController searchController = TextEditingController();
+  List<Map<String, dynamic>> filteredRecipes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    recipes = loadRecipes();
+  }
+
+  Future<List<Map<String, dynamic>>> loadRecipes() async {
+    final String jsonString =
+        await rootBundle.loadString('assets/recipes.json');
+    final List<dynamic> jsonResponse = json.decode(jsonString);
+    return jsonResponse
+        .map((recipe) => recipe as Map<String, dynamic>)
+        .toList();
+  }
+
+  void _filterRecipes(String query) {
+    if (query.isEmpty) {
+      setState(() {
+        filteredRecipes = [];
+      });
+    } else {
+      setState(() {
+        filteredRecipes = filteredRecipes
+            .where((recipe) =>
+                recipe['name']!.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -122,6 +162,8 @@ class HomeScreen extends StatelessWidget {
             ),
             const SizedBox(height: 40),
             TextField(
+              controller: searchController,
+              onChanged: _filterRecipes,
               decoration: InputDecoration(
                 hintText: 'Search',
                 filled: true,
@@ -160,106 +202,175 @@ class HomeScreen extends StatelessWidget {
               child: Padding(
                 padding:
                     const EdgeInsets.only(top: 2), // Reduce top margin by 15px
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, // 2 cards per row
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    childAspectRatio:
-                        167 / 218, // Adjust to match card dimensions
-                  ),
-                  itemCount: recipes.length,
-                  itemBuilder: (context, index) {
-                    final recipe = recipes[index];
-                    return GestureDetector(
-                      onTap: () {
-                        // Navigate to recipe details
-                      },
-                      child: SizedBox(
-                        height: 218, // Set card height
-                        width: 167, // Set card width
-                        child: Card(
-                          color: const Color(0xFFE0E0E0),
-                          elevation: 2,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Stack(
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                child: FutureBuilder<List<Map<String, dynamic>>>(
+                  future: recipes,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    }
+
+                    final recipesData = snapshot.data ?? [];
+                    if (filteredRecipes.isEmpty) {
+                      filteredRecipes = recipesData;
+                    }
+
+                    return GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2, // 2 cards per row
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        childAspectRatio:
+                            167 / 218, // Adjust to match card dimensions
+                      ),
+                      itemCount: filteredRecipes.length,
+                      itemBuilder: (context, index) {
+                        final recipe = filteredRecipes[index];
+                        return GestureDetector(
+                          onTap: () {
+                            // Navigate to recipe details
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    RecipeDetailScreen(recipe: recipe),
+                              ),
+                            );
+                          },
+                          child: SizedBox(
+                            height: 218, // Set card height
+                            width: 167, // Set card width
+                            child: Card(
+                              color: const Color(0xFFE0E0E0),
+                              elevation: 2,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Stack(
                                 children: [
-                                  ClipRRect(
-                                    borderRadius: const BorderRadius.vertical(
-                                      top: Radius.circular(10),
-                                    ),
-                                    child: Image.asset(
-                                      recipe['image']!,
-                                      height: 174,
-                                      width: double.infinity,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Center(
-                                      child: Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 8),
-                                        child: Text(
-                                          recipe['name']!,
-                                          textAlign: TextAlign.center,
-                                          style: const TextStyle(
-                                            color: Color(0xFF1A1A1A),
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w900,
-                                            fontFamily: 'Inter',
-                                          ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius:
+                                            const BorderRadius.vertical(
+                                          top: Radius.circular(10),
+                                        ),
+                                        child: Image.asset(
+                                          recipe['image']!,
+                                          height: 174,
+                                          width: double.infinity,
+                                          fit: BoxFit.cover,
                                         ),
                                       ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Positioned(
-                                top: 10,
-                                right: 2,
-                                child: Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 4, horizontal: 8),
-                                  decoration: BoxDecoration(
-                                    color: const Color(0xFFFF4B3E),
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.timer,
-                                        size: 14,
-                                        color: Color(0xFFF5F5F5),
-                                      ),
-                                      const SizedBox(width: 4),
-                                      Text(
-                                        recipe['time']!,
-                                        style: const TextStyle(
-                                          fontSize: 12,
-                                          color: Color(0xFFF5F5F5),
-                                          fontFamily: 'Inter',
+                                      Expanded(
+                                        child: Center(
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                vertical: 10),
+                                            child: Text(
+                                              recipe['name']!,
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 14),
+                                            ),
+                                          ),
                                         ),
                                       ),
                                     ],
                                   ),
-                                ),
+                                ],
                               ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ),
+                        );
+                      },
                     );
                   },
                 ),
               ),
-            )
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class RecipeDetailScreen extends StatefulWidget {
+  final Map<String, dynamic> recipe;
+
+  const RecipeDetailScreen({super.key, required this.recipe});
+
+  @override
+  _RecipeDetailScreenState createState() => _RecipeDetailScreenState();
+}
+
+class _RecipeDetailScreenState extends State<RecipeDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final recipe = widget.recipe;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe['name']!),
+        backgroundColor: Colors.red,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                recipe['image']!,
+                fit: BoxFit.cover,
+                height: 250,
+                width: double.infinity,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Ingredients:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+              const SizedBox(height: 10),
+              ...List<Widget>.from(
+                recipe['ingredients'].map<Widget>((ingredient) {
+                  return Text(
+                    ingredient!,
+                    style: const TextStyle(fontSize: 16),
+                  );
+                }),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Instructions:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+              const SizedBox(height: 10),
+              ...List<Widget>.from(
+                recipe['instructions'].map<Widget>((instruction) {
+                  return Text(
+                    instruction!,
+                    style: const TextStyle(fontSize: 16),
+                  );
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
